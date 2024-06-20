3e7a8161e3f34650bf2ac2d5326ffc582afdd968
UbdaNam <abdugreat2@gmail.com>
2024-06-20 15:47:15 +0300

    Add card button transition

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,24 +1,34 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
 import '../stylesheets/countryCard.css';
 
 const CountryCard = ({ country }) => {
+  const navigate = useNavigate();
+
+  const handleClick = (countryName) => {
+    navigate('/countries/' + countryName);
+  };
   return (
-    <Link to={`countries/${country.name}`}>
-      <li className='list-item'>
-        <div className='card-title'>
-          <div className='flag'>
-            <img src={country.flags.svg} alt={`${country.name} flag`} />
-          </div>
-          <h2>{country.name}</h2>
+    <li className='list-item'>
+      <div className='card-title'>
+        <div className='flag'>
+          <img src={country.flags.svg} alt={`${country.name} flag`} />
         </div>
-        <div className='card-body'>
+        <h2>{country.name}</h2>
+      </div>
+      <div className='card-body'>
+        <div>
           <h3>Region: {country.region}</h3>
           <p>
             Area: {country?.area?.toLocaleString()} Km<sup>2</sup>
           </p>
         </div>
-      </li>
-    </Link>
+        <button onClick={() => handleClick(country.name)} className='more-btn'>
+          <span>Details</span>
+          <FaArrowRight />
+        </button>
+      </div>
+    </li>
   );
 };
 
